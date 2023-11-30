451a14d4c07c0db5d53c4e377d80b74c45169db9
fathi bouslama <bouslamafathi90@gmail.com>
2023-11-30 22:40:15 +0100

    Sorting by date and CastleName-DiaryEntryPage

--- app/src/main/java/com/example/oldcastellovers/UI/activities/DiaryEntryActivity.java
+++ app/src/main/java/com/example/oldcastellovers/UI/activities/DiaryEntryActivity.java
@@ -11,7 +11,13 @@ import com.example.oldcastellovers.R;
 import com.example.oldcastellovers.database.DataBaseHelper;
 import com.example.oldcastellovers.database.models.DiaryEntryModel;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 
 public class DiaryEntryActivity extends AppCompatActivity {
 
@@ -21,7 +27,6 @@ public class DiaryEntryActivity extends AppCompatActivity {
 
 
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.display_entry_recycler_view);
@@ -31,8 +36,37 @@ public class DiaryEntryActivity extends AppCompatActivity {
 
         List<DiaryEntryModel> diaryEntryModels = dataBaseHelper.getCastleDetailsWithMediaPath();
 
+        sortDiaryEntryModels(diaryEntryModels);
+
         adapter = new DiaryEntryAdapter(this, diaryEntryModels);
         entryRecyclerView.setLayoutManager(new LinearLayoutManager(this));
         entryRecyclerView.setAdapter(adapter);
+    }
+
+    // sorting by entry date and castle name
+    private void sortDiaryEntryModels(List<DiaryEntryModel> diaryEntryModels) {
+        Collections.sort(diaryEntryModels, new Comparator<DiaryEntryModel>() {
+            SimpleDateFormat dateFormat = new SimpleDateFormat("MMM dd yyyy", Locale.getDefault());
+
+            @Override
+            public int compare(DiaryEntryModel model1, DiaryEntryModel model2) {
+                try {
+                    Date date1 = dateFormat.parse(model1.getDate());
+                    Date date2 = dateFormat.parse(model2.getDate());
+
+                    // Compare dates first (descending order - most recent first)
+                    int dateComparison = date2.compareTo(date1);
+                    if (dateComparison != 0) {
+                        return dateComparison;
+                    }
+
+                    // If dates are the same, compare castle names alphabetically
+                    return model1.getCastleName().compareToIgnoreCase(model2.getCastleName());
+                } catch (ParseException e) {
+                    e.printStackTrace();
+                    return 0; // Handle the exception according to your needs
+                }
+            }
+        });
     }
 }
