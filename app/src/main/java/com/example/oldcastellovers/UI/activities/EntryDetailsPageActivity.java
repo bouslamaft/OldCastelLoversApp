6faaec67a27a7b0566105510d3a9bf593a9d41b6
chima-cdn <chimacenation@gmail.com>
2023-12-07 19:23:35 +0100

    able to display images not video and problem of displaying twice insted of once

--- app/src/main/java/com/example/oldcastellovers/UI/activities/EntryDetailsPageActivity.java
+++ app/src/main/java/com/example/oldcastellovers/UI/activities/EntryDetailsPageActivity.java
@@ -5,6 +5,7 @@ import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
@@ -53,6 +54,8 @@ public class EntryDetailsPageActivity extends AppCompatActivity {
         // Fetch the DiaryEntryModel from the database based on entryId
         DiaryEntryModel entryDetails = dataBaseHelper.getDiaryEntryDetails(entryId);
 
+        Log.d("media", "Media path " + entryDetails.getMediaPath());
+
         // Display the values in the UI components
         if (entryDetails != null) {
             datePickerButton.setText(entryDetails.getDate());
@@ -68,62 +71,14 @@ public class EntryDetailsPageActivity extends AppCompatActivity {
 //        entryRecyclerView.setLayoutManager(new LinearLayoutManager(this));
 //        entryRecyclerView.setAdapter(adapter);
 
-        List<DiaryEntryModel> diaryEntryModels = dataBaseHelper.getCastleDetailsWithMediaPath();
+        //DiaryEntryModel diaryEntryModels = dataBaseHelper.getDiaryEntryDetails(entryId);
 
         // Set up RecyclerView with a grid layout manager
         GridLayoutManager layoutManager = new GridLayoutManager(this, 3);
         diary_recycler_view.setLayoutManager(layoutManager);
 
-        adapter = new DiaryEntryMediaAdapter(this, diaryEntryModels);
+        adapter = new DiaryEntryMediaAdapter( this, entryDetails.getMediaPath());
         diary_recycler_view.setAdapter(adapter);
 
     }
-
-    private void addMediaItemToLayout(Uri mediaUri) {
-        // Create a new LinearLayout to hold the media item and remove button
-        LinearLayout mediaItemLayout = new LinearLayout(this);
-        mediaItemLayout.setOrientation(LinearLayout.HORIZONTAL);
-
-        // Create an ImageView for the media
-        ImageView imageView = new ImageView(this);
-        LinearLayout.LayoutParams imageParams = new LinearLayout.LayoutParams(
-                300, 300  // Set a fixed size for the media item (adjust as needed)
-        );
-        imageView.setLayoutParams(imageParams);
-
-        // Check if the media is a video and set the thumbnail accordingly
-        if (isVideoFile(mediaUri.getPath())) {
-            loadVideoThumbnail(imageView, mediaUri);
-        } else {
-            imageView.setImageURI(mediaUri);
-        }
-
-        // Add the ImageView and Remove button to the mediaItemLayout
-        mediaItemLayout.addView(imageView);
-
-
-        // Add the mediaItemLayout to the mediaLayout
-        LinearLayout mediaLayout = findViewById(R.id.mediaLayout);
-        mediaLayout.addView(mediaItemLayout);
-    }
-
-
-
-    private void loadVideoThumbnail(ImageView imageView, Uri videoUri) {
-        Glide.with(this)
-                .load(videoUri)
-                .into(imageView);
-    }
-
-    protected boolean isVideoFile(String path) {
-        // Implement this method to check if the file at the given path is a video file
-        // You can use the file extension or other methods to determine the file type
-        // For example, you can check if the path ends with ".mp4"
-        return path.toLowerCase().endsWith(".mp4");
-    }
-
-
-
-
-
 }
--- app/src/main/java/com/example/oldcastellovers/UI/adapters/DiaryEntryMediaAdapter.java
+++ app/src/main/java/com/example/oldcastellovers/UI/adapters/DiaryEntryMediaAdapter.java
@@ -1,11 +1,6 @@
 package com.example.oldcastellovers.UI.adapters;
 
 import android.content.Context;
-import android.content.Intent;
-import android.graphics.Bitmap;
-import android.media.ThumbnailUtils;
-import android.net.Uri;
-import android.provider.MediaStore;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -19,21 +14,17 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.request.RequestOptions;
 import com.example.oldcastellovers.R;
-import com.example.oldcastellovers.UI.GalleryItem;
-import com.example.oldcastellovers.UI.VideoGalleryItem;
-import com.example.oldcastellovers.UI.activities.MediaViewActivity;
 import com.example.oldcastellovers.database.models.DiaryEntryModel;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class DiaryEntryMediaAdapter extends RecyclerView.Adapter<DiaryEntryMediaAdapter.DiaryViewHolder> {
 
     private Context context;
-    private List<DiaryEntryModel> diaryEntries;
+    private ArrayList<String> diaryEntries;
 
-    public DiaryEntryMediaAdapter(Context context, List<DiaryEntryModel> diaryEntries) {
+    public DiaryEntryMediaAdapter(Context context, ArrayList<String> diaryEntries) {
         this.context = context;
         this.diaryEntries = diaryEntries;
     }
@@ -48,16 +39,16 @@ public class DiaryEntryMediaAdapter extends RecyclerView.Adapter<DiaryEntryMedia
 
     @Override
     public void onBindViewHolder(@NonNull DiaryViewHolder holder, int position) {
-        DiaryEntryModel entry = diaryEntries.get(position);
+        String entry = diaryEntries.get(position);
 
-        ArrayList<String> mediaPaths = new ArrayList<>(Arrays.asList(entry.getMediaPath().spl     //.split(",")));
-        Log.d("imagepath", "onBindViewHolder: " + imagePath);
-        Glide.with(context)
-                .load(imagePath)
-                .placeholder(R.drawable.homecastlepic) // Replace with your placeholder image
-                .into(holder.imageView);
+        // Clear the existing content in the ImageView
+        holder.imageView.setImageDrawable(null);
+
+        // Load the media content into the ImageView using Glide
+        loadImageIntoImageView(entry, holder.imageView);
 
 
+        Log.d("test333", "path " + entry);
     }
 
     @Override
@@ -74,6 +65,17 @@ public class DiaryEntryMediaAdapter extends RecyclerView.Adapter<DiaryEntryMedia
             imageView = itemView.findViewById(R.id.imageView);
             videoView = itemView.findViewById(R.id.videoView);
         }
+    }
+
+    // Helper method to load the image into the ImageView using Glide
+    private void loadImageIntoImageView(String mediaPath, ImageView imageView) {
+        // You can customize RequestOptions as needed (e.g., placeholder, error)
+        RequestOptions requestOptions = new RequestOptions();
+
+        Glide.with(context)
+                .load(mediaPath)
+                .apply(requestOptions)
+                .into(imageView);
     }
 }
 
